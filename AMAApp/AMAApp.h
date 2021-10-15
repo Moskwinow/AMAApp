bd97cffd835cf888928babf7b56125c43f8e3290
Maksym <maksym.vechirko@axiomc.com>
2021-10-15 10:03:35 +0300

    Initial Commit

---
+++ AMAApp/AMAApp.h
@@ -0,0 +1,18 @@
+//
+//  AMAApp.h
+//  AMAApp
+//
+//  Created by Maksym on 15/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AMAApp.
+FOUNDATION_EXPORT double AMAAppVersionNumber;
+
+//! Project version string for AMAApp.
+FOUNDATION_EXPORT const unsigned char AMAAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMAApp/PublicHeader.h>
+
+
